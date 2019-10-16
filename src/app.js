2b1e7a68f5530fb2ef6933d39472e8458fe0d8f1
cai <cairenjie@hzed.com>
2019-10-16 18:03:28 +0800

    updata 10-16 1018:03

--- src/app.js
+++ src/app.js
@@ -1,43 +1,30 @@
-
-
 var Koa = require('koa');
-
+const bodyParser = require('koa-bodyparser')
 var router = require('koa-router')();  /*引入是实例化路由** 推荐*/
 
 //实例化
 var app = new Koa();
+app.use(bodyParser())
 
-router.get('/', async (ctx) => {
-  ctx.body = "首页";
-
+router.post('/user/register', async (ctx, next) => {
+  let { name, password } = ctx.request.body
+  if (name === 'cai' && password === '123') {
+    ctx.set('Content-Type', 'application/json;charset=utf-8')
+    ctx.response.body = {
+      code: "0",
+      message: "成功"
+    }
+  } else {
+    ctx.response.body = '账号信息错误'
+  }
 })
-
-router.get('/news', async (ctx) => {
-  ctx.body = "新闻列表页面";
-
-})
-
 //获取get传值
 //http://localhost:3002/newscontent?aid=123
 
 router.get('/newscontent', async (ctx) => {
 
   //从ctx中读取get传值
-
-  console.log(ctx.query);  //{ aid: '123' }       获取的是对象   用的最多的方式      ******推荐
-
-  console.log(ctx.querystring);  //aid=123&name=zhangsan      获取的是一个字符串
-
-  console.log(ctx.url);   //获取url地址
-
-  //ctx里面的request里面获取get传值
-
-  console.log(ctx.request.url);
-  console.log(ctx.request.query);   //{ aid: '123', name: 'zhangsan' }  对象
-  console.log(ctx.request.querystring);   //aid=123&name=zhangsan
-
-
-  // ctx.body = { "aa": "11" };
+  let C = ctx;
   let url = ctx.url;
   //从request中获取GET请求
   let request = ctx.request;
@@ -46,7 +33,9 @@ router.get('/newscontent', async (ctx) => {
   //从上下文中直接获取
   let ctx_query = ctx.query;
   let ctx_querystring = ctx.querystring;
+  ctx.set('Content-Type', 'application/json;charset=utf-8')
   ctx.body = {
+    C: C,
     url,
     req_query,
     req_querystring,
@@ -64,4 +53,3 @@ const port = 3002
 app.listen(port, () => {
   console.log(`koa2 is working on port ${port}...`)
 })
-
